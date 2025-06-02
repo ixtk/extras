cdc6ac96155868e01609a808581d87178e137e35
ixtk <ikhcamot@gmail.com>
2025-06-02 14:11:34 +0400

    Get started on DB connection

--- commerce/index.js
+++ commerce/index.js
@@ -1,5 +1,7 @@
 import express from "express";
 import products from "./products.json" with { type: "json" }
+import mongoose from "mongoose";
+import { Product } from "./models.js";
 
 const app = express();
 
@@ -8,12 +10,27 @@ app.use("/assets", express.static("./assets"));
 app.set("view engine", "ejs");
 // to change view directory: app.set('views', ...)
 
+app.get("/", (req, res) =>{
+  res.redirect("/products")
+})
+
 app.get("/products", (req, res) => {
   res.render("products", {
     title: "Product List",
     products: products,
   });
 });
+
+app.post("/products/new", (req, res) => {
+  // TODO...
+  console.log("POST form")
+})
+
+app.get('/products/new', (req, res) => {
+  res.render("addProduct",{
+    title: "Add a new product"
+  })
+})
 
 app.get("/products/:productId", (req, res) => {
   // req.params is an object
@@ -36,6 +53,10 @@ app.get("/products/:productId", (req, res) => {
   });
 });
 
-app.listen(3001, () => {
+const connection = await mongoose.connect('mongodb://localhost:27017/commerce')
+
+console.log("Connected to the DB: commerce")
+
+app.listen(3002, () => {
   console.log("Running!");
 });
---
+++ commerce/models.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const productSchema = mongoose.Schema({
+  title: { type: String, required: true },
+  price: { type: Number, required: true },
+  salePrice: Number,
+  category: { type: String, required: true },
+  imageUrl: { type: String, required: true },
+});
+
+export const Product = mongoose.model("Product", productSchema)
