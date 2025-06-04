30f521a933f664b3a4a0b60b0449a6b3a9f53cac
ixtk <ikhcamot@gmail.com>
2025-06-04 17:29:16 +0400

    Work on products

--- commerce/index.js
+++ commerce/index.js
@@ -5,17 +5,17 @@ import { Product } from "./models.js";
 const app = express();
 
 app.use("/assets", express.static("./assets"));
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 // to change view directory: app.set('views', ...)
 
-app.get("/", (req, res) =>{
-  res.redirect("/products")
-})
+app.get("/", (req, res) => {
+  res.redirect("/products");
+});
 
 app.get("/products", async (req, res) => {
-  const products = await Product.find()
+  const products = await Product.find();
 
   res.render("products", {
     title: "Product List",
@@ -24,35 +24,43 @@ app.get("/products", async (req, res) => {
 });
 
 app.post("/products/new", async (req, res) => {
-  const newProductValues = req.body
-  
-  const newProduct = await Product.create(newProductValues)
+  const newProductValues = req.body;
+
+  const newProduct = await Product.create(newProductValues);
 
   res.redirect(`/products/${newProduct.id}`);
-})
+});
 
-app.get('/products/new', (req, res) => {
-  res.render("addProduct",{
-    title: "Add a new product"
-  })
-})
-
-app.get("/products/:productId", async (req, res) => {
-  // req.params is an object
-  console.log(req.params)
-
-  const productId = req.params.productId
-
-  const foundProduct = await Product.findById(productId)
-
-  res.render("singleProduct", {
-    product: foundProduct
+app.get("/products/new", (req, res) => {
+  res.render("addProduct", {
+    title: "Add a new product",
   });
 });
 
-const connection = await mongoose.connect('mongodb://localhost:27017/commerce')
+app.get("/products/:productId", async (req, res) => {
+  // req.params is an object
+  console.log(req.params);
 
-console.log("Connected to the DB: commerce")
+  const productId = req.params.productId;
+
+  const foundProduct = await Product.findById(productId);
+
+  res.render("singleProduct", {
+    product: foundProduct,
+  });
+});
+
+app.post("/products/:productId", async (req, res) => {
+  const productId = req.params.productId;
+
+  await Product.findByIdAndDelete(productId);
+
+  res.redirect("/products")
+});
+
+const connection = await mongoose.connect("mongodb://localhost:27017/commerce");
+
+console.log("Connected to the DB: commerce");
 
 app.listen(3002, () => {
   console.log("Running!");
